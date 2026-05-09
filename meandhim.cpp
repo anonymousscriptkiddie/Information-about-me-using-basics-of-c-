8599f8d86a163eb9dfdc25e24116ae4b6a4d4ddb
s46bbbbbiu7otn85rh4 <99702178+anonymousscriptkiddie@users.noreply.github.com>
2026-05-09 14:19:37 +0800

    Update meandhim.cpp

--- meandhim.cpp
+++ meandhim.cpp
@@ -6,9 +6,11 @@ using namespace std;
 
 namespace XY {
     int zach = 22; // have lacked 1 more genetic variable
+    string geneticPossibility0 ="Standard twins";
 }
 namespace XX {
     int precious = 23; // sufficient genetic variable
+    string geneticPossibility1 = "Faternal twins";
 }
 void geneticGambled() {
     char F = 100; // Full Identical twins
@@ -18,6 +20,20 @@ void geneticGambled() {
   void ages(){
       string agers="16 and 17";
       cout << "Zach and Precious ages are: " << agers << endl;
+  }
+  void geneticGlitch() {
+      string id = "identical twins";
+      cout << id << endl;
+  }
+  void numberTwinss() {
+        typedef string num;
+    num twinNumber[] = {"single", "one", "two", "three", "four", "five"};
+    cout << twinNumber[0] << "\n";
+     cout << twinNumber[1] << "\n";
+      cout << twinNumber[2] << "\n";
+       cout << twinNumber[3] << "\n";
+        cout << twinNumber[4] << "\n";
+         cout << twinNumber[5] << "\n";
   }
     
 
@@ -30,6 +46,8 @@ typedef int nomnom; // can be used to declare an integer
 
 int main()
 {
+  cout << "possibilities of childrens: "<< endl;
+        
     //Data
     person_name zach1= "Zachary Bates";
     person_name precious = "Precious Prado";
@@ -47,6 +65,10 @@ int main()
     cout << "Wanna know the father?: "<< std::endl;
     getline(cin, zach1);
     cout << "The name of the father is: " << zz <<std::endl;
+    
+    cout << "Wanna know the name of the mother? "<<std::endl;
+    getline(cin, precious);
+    cout << "the name of the mother is "<< precious << std::endl;
     
     typedef string choices;
     choices userSelection;
@@ -96,7 +118,27 @@ int main()
         default:
         cout << "enter the choices, not any other" << endl;
     }
-   
+    cin.ignore();
+     string twinType;
+     cout << "Gamble 1, about Twin Type (choose syrongest gene of (f or m or type f m)) ";
+     cin >> twinType;
+     
+     if(twinType == "f"){
+        cout << XX::geneticPossibility1;
+     }
+     else if(twinType == "m"){
+         cout << XY::geneticPossibility0;
+     }
+     else if(twinType == "f or m" || twinType == "f m"){
+         cout << "both has possibiliies to be standard or faternal";
+     }
+     else {
+         geneticGlitch();
+         for(int i = 1; i <= 50; i++){
+             std::cout << "identical twin\n"<< endl;
+         }
+     }
+     
     
     return 0;
 }
